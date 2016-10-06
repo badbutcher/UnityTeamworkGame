71f4eb58925be9c17c136f023d7bf76b862dfbe6
badbutcher <pavel.p.n@abv.bg>
2016-10-06 22:51:52 +0300

    test

---
+++ UnityTeamworkGame/Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("asdadasd");
+        for (int i = 0; i < 111; i++)
+        {
+            transform.Translate(1, 1, 1);
+        }
+	}
+}
