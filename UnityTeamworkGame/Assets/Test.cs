2c4414da4dcaefd27a2c83035487e3ae7660f504
badbutcher <pavel.p.n@abv.bg>
2016-10-06 22:58:10 +0300

    Revert "more tests"
    This reverts commit 696f23c1a994c998fc1de2a908025744310ea7e0.

--- UnityTeamworkGame/Assets/Test.cs
+++ UnityTeamworkGame/Assets/Test.cs
@@ -3,4 +3,18 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("asdadasd");
+        for (int i = 0; i < 111; i++)
+        {
+            transform.Translate(1, 1, 1);
+        }
+	}
 }
