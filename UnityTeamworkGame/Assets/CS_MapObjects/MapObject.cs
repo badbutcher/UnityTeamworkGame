0e1c1029c43e7d931167a9db355361e65ad81877
Simeon Tzvetkov <simeon.s.tzvetkov@gmail.com>
2016-10-09 20:51:59 +0300

    Add CS classes to the project

---
+++ UnityTeamworkGame/Assets/CS_MapObjects/MapObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityTeamworkGame.CS_Interfaces;
+
+namespace UnityTeamworkGame.CS_MapObjects
+{
+    public abstract class MapObject
+    {
+        protected MapObject(int x, int y)
+        {
+            this.LocationX = x;
+            this.LocationY = y;
+        }        
+
+        // properties
+        public int LocationX { get; private set; }
+        public int LocationY { get; private set; }
+    }
+}
