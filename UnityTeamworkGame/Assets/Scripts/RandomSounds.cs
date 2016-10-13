a452acf4f0a6ca2d7a420161bc5d20a3c3d3d3b5
badbutcher <pavel.p.n@abv.bg>
2016-10-13 13:19:21 +0300

    Fix?

---
+++ UnityTeamworkGame/Assets/Scripts/RandomSounds.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RandomSounds : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip[] sounds;
+    public int rnd;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+
+        if (!(SceneManager.GetActiveScene().buildIndex == 0))
+        {
+            StartCoroutine(RandomSound());
+        }
+        source.volume = SoundSave.currentSoundEffectsValue;
+    }
+
+    IEnumerator RandomSound()
+    {
+        while (true)
+        {
+            rnd = Random.Range(0, sounds.Length);
+            yield return new WaitForSeconds(Random.Range(20,30));
+            source.PlayOneShot(sounds[rnd]);
+        }
+    }
+}
