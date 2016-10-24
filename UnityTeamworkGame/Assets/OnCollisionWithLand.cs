e4c65a01064b2ab708a5e2e86a6730f60414d952
NightFer <nightferfn@gmail.com>
2016-10-24 18:51:58 +0300

    Map Corrections

---
+++ UnityTeamworkGame/Assets/OnCollisionWithLand.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandSoundController : MonoBehaviour
+{
+    public AudioClip land;    
+                             
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = land;
+    }
+
+    void OnCollisionEnter()  
+    {
+        GetComponent<AudioSource>().Play();
+    }
+    
+}
