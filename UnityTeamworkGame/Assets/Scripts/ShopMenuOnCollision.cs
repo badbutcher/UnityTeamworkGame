7fa52c3c8af7f6e7bf7ee66e2682876e3af51a49
NightFer <nightferfn@gmail.com>
2016-10-26 15:12:49 +0300

    last changes
    .

---
+++ UnityTeamworkGame/Assets/Scripts/ShopMenuOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopMenuOnCollision : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnCollisionEnter2D (Collision2D shopMenu)
+    {
+        if (shopMenu.gameObject.tag == "Character")
+        {
+            
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
