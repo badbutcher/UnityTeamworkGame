620468587f2bc1cd9ef7f5a64b7a01fb54aa0277
badbutcher <pavel.p.n@abv.bg>
2016-10-26 11:53:28 +0300

    Better shop menu

--- UnityTeamworkGame/Assets/Scripts/UI/ShopButtons.cs
+++ UnityTeamworkGame/Assets/Scripts/UI/ShopButtons.cs
@@ -1,54 +1,73 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopButtons : MonoBehaviour
 {
+    public Text CurrentMenu;
+    public Text PlayerGold;
+    public Text PlayerHp;
+    public Text PlayerCannonBalls;
     public GameObject WeaponsPanel;
-    public GameObject SuppliesPanel;
-    public GameObject UpgradesPanel;
-    public GameObject PotionsPanel;
-    public GameObject InnPanel;
+    public GameObject UpdatePanel;
     public GameObject RepairShipPanel;
+    public GameObject QuestPanel;
     public GameObject ExitShop;
+    private AudioSource source;
+    public AudioClip[] Sounds;
+
+    //Test
+
+    public float Gold;
+    public float Hp;
+    public float MaxHp;
+    public float CannonBalls;
+    public float MaxCannonBalls;
+
+    void Awake()
+    {
+        this.source = this.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        this.PlayerGold.text = " - " + this.Gold;
+        this.PlayerHp.text = " - " + this.Hp + " / " + this.MaxHp;
+        this.PlayerCannonBalls.text = " - " + this.CannonBalls + " / " + this.MaxCannonBalls;
+    }
 
     private void Start()
     {
+        this.source.PlayOneShot(this.Sounds[0]);
+        this.CurrentMenu.text = "Welcome";
         this.Reset();
     }
 
     public void ShowWeapons()
     {
+        this.CurrentMenu.text = "Weapon Supplies";
         this.Reset();
         this.WeaponsPanel.SetActive(true);
     }
 
-    public void ShowSupplies()
+    public void ShowUpdates()
     {
+        this.CurrentMenu.text = "Ship Updates";
         this.Reset();
-        this.SuppliesPanel.SetActive(true);
-    }
-
-    public void ShowUpgrades()
-    {
-        this.Reset();
-        this.UpgradesPanel.SetActive(true);
-    }
-
-    public void ShowPotions()
-    {
-        this.Reset();
-        this.PotionsPanel.SetActive(true);
-    }
-
-    public void ShowInn()
-    {
-        this.Reset();
-        this.InnPanel.SetActive(true);
+        this.UpdatePanel.SetActive(true);
     }
 
     public void ShowRepairShip()
     {
+        this.CurrentMenu.text = "Ship Repair";
         this.Reset();
         this.RepairShipPanel.SetActive(true);
+    }
+
+    public void ShowQuest()
+    {
+        this.CurrentMenu.text = "Tavern";
+        this.Reset();
+        this.QuestPanel.SetActive(true);
     }
 
     public void Exit()
@@ -60,10 +79,96 @@ public class ShopButtons : MonoBehaviour
     private void Reset()
     {
         this.WeaponsPanel.SetActive(false);
-        this.SuppliesPanel.SetActive(false);
-        this.UpgradesPanel.SetActive(false);
-        this.PotionsPanel.SetActive(false);
-        this.InnPanel.SetActive(false);
+        this.UpdatePanel.SetActive(false);
+        this.QuestPanel.SetActive(false);
         this.RepairShipPanel.SetActive(false);
+    }
+
+    public void BuyCannonBalls()
+    {
+        if (this.Gold >= 50 && this.MaxCannonBalls > this.CannonBalls)
+        {
+            this.source.PlayOneShot(this.Sounds[1]);
+            this.Gold -= 50;
+            this.CannonBalls++;
+        }
+    }
+
+    public void BuyExtraCannon()
+    {
+        //TODO
+    }
+
+    public void BuyCannonsReloadSpeed()
+    {
+        //TODO
+    }
+
+    public void ImproveSpeed()
+    {
+        //TODO
+    }
+
+    public void ImproveHealth()
+    {
+        if (this.Gold >= 100)
+        {
+            this.source.PlayOneShot(this.Sounds[1]);
+            this.Gold -= 100;
+            this.Hp += 50;
+            this.MaxHp += 50;
+        }
+    }
+
+    public void ImproveStorage()
+    {
+        if (this.Gold >= 100)
+        {
+            this.source.PlayOneShot(this.Sounds[1]);
+            this.Gold -= 100;
+            this.MaxCannonBalls += 5;
+        }
+    }
+
+    public void Repair25()
+    {
+        if (this.Gold >= 20 && this.Hp < this.MaxHp)
+        {
+            this.source.PlayOneShot(this.Sounds[1]);
+            this.Gold -= 20;
+            this.Hp += 25;
+            this.FixHealth();
+        }
+    }
+
+    public void Repair50()
+    {
+        if (this.Gold >= 40 && this.Hp < this.MaxHp)
+        {
+            this.source.PlayOneShot(this.Sounds[1]);
+            this.Gold -= 40;
+            this.Hp += 50;
+            this.FixHealth();
+        }
+    }
+
+    public void Repair75()
+    {
+        if (this.Gold >= 60 && this.Hp < this.MaxHp)
+        {
+            this.source.PlayOneShot(this.Sounds[1]);
+            this.Gold -= 60;
+            this.Hp += 75;
+            this.FixHealth();
+        }
+    }
+
+    private void FixHealth()
+    {
+        if (this.Hp > this.MaxHp)
+        {
+            this.source.PlayOneShot(this.Sounds[1]);
+            this.Hp = this.MaxHp;
+        }
     }
 }
