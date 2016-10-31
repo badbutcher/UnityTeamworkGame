406aa676e71d85dc6774b829d592331e2ebeb1a3
NightFer <nightferfn@gmail.com>
2016-10-31 20:47:09 +0200

    Simple Quest System
    Added simple quest system scripts. Need to impliment them inside the
    game and work on them more.

---
+++ UnityTeamworkGame/Assets/Quest System/Scripts/Interfaces/IQuestIdentifier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace QuestSystem
+{
+    public interface IQuestIdentifier
+    {
+        int ID { get; }
+        int SourceID { get; }
+    }
+}
---
+++ UnityTeamworkGame/Assets/Quest System/Scripts/Objectives/LocationObjective.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+namespace QuestSystem
+{
+    public class LocationObjective : IQuestObjective
+    {
+        private string title;
+        private string description;
+        private bool isComplete;
+
+        private Location targetLocation;    //zone, 2d cord, 3d cord
+
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+        }
+
+        public bool IsComplete
+        {
+            get
+            {
+                return isComplete;
+            }
+        }
+
+
+        public void CheckProgress()
+        {
+            //if players location is equal to our target location then we are complete and we have finished objective
+            if (Player.GetLocation.Compare(targetLocation))
+                isComplete = true;
+            else
+                isComplete = false;
+        }
+
+        public void UpdateProgress()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
