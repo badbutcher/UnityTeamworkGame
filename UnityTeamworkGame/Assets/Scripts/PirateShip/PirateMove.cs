8fcb55467b6b069081fe846745ea5c8621db6dea
Simeon Tzvetkov <simeon.s.tzvetkov@gmail.com>
2016-11-08 23:26:31 +0200

    PirateMove updated

--- UnityTeamworkGame/Assets/Scripts/PirateShip/PirateMove.cs
+++ UnityTeamworkGame/Assets/Scripts/PirateShip/PirateMove.cs
@@ -1,5 +1,6 @@
-
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityTeamworkGame.CS_Common;
 
 public class PirateMove : MonoBehaviour
 {
@@ -8,9 +9,19 @@ public class PirateMove : MonoBehaviour
     public int CurrentPoint;
     private float maxTurn = 0.5f;
     public float moveSpeed;
+    private int[] destination;
+    private float X;
+    private float Y;
+    private List<float[]> path;
+    private bool isInRange;
 
     private void Start()
     {
+        this.isInRange = false;
+        this.path = new List<float[]>();
+        this.destination = new int[2] { 100, 100 };
+        this.X = 1f;
+        this.Y = 1f;
         this.moveSpeed = PlayerStats.PlayerMoveSpeed + Random.Range(-0.2f, 0.5f);
         this.Reset();
         this.Sprites[0].SetActive(true);
@@ -88,5 +99,50 @@ public class PirateMove : MonoBehaviour
         this.Sprites[5].SetActive(false);
         this.Sprites[6].SetActive(false);
         this.Sprites[7].SetActive(false);
+    }
+
+    // Ship movement 
+
+    private void GetNextPointPosition(bool playerHasMoved, out float[] nextPositionCoords)
+    {
+        nextPositionCoords = new float[2];
+        int[] target = new int[2];
+        bool wasInRange = this.isInRange;
+        CheckInRange(PlayerMove.X, PlayerMove.Y);
+        if (this.isInRange)
+        {
+            target = Map.ConvertLocationToCoord(new float[2] {PlayerMove.X, PlayerMove.Y});
+            if (this.path.Count == 0 || playerHasMoved)  
+            {
+                int[] start = new int[2];
+                start = Map.ConvertLocationToCoord(new float[2] { this.X, this.Y});
+                Map.GetPathToTarget(start, target, out this.path);
+            }
+        }
+        else if (wasInRange || this.path.Count == 0)
+        {
+            target[0] = this.destination[0];
+            target[1] = this.destination[1];
+            int[] start = new int[2];
+            start = Map.ConvertLocationToCoord(new float[2] { this.X, this.Y });
+            Map.GetPathToTarget(start, target, out this.path);
+        }
+        if (this.path.Count > 0)
+        {
+            nextPositionCoords[0] = this.path[0][0];
+            nextPositionCoords[1] = this.path[0][1];
+            this.path.RemoveAt(0);
+        }
+        else
+        {
+            nextPositionCoords[0] = this.X;
+            nextPositionCoords[1] = this.Y;
+        }
+    }
+
+    private void CheckInRange(float PlayerShipPositionX, float PlayerShipPositionY)
+    {
+        int distance = (int)Mathf.Sqrt(Mathf.Pow((PlayerShipPositionX - this.X), 2) + Mathf.Pow((PlayerShipPositionY - this.Y), 2));
+        this.isInRange = distance <= Map.DETECT_RANGE;
     }
 }
